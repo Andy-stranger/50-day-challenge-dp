290472f926e8252d7b991f5d2af47693eced79b1
DESKTOP-MHCL4AU\User <anandhagani2001@gmail.com>
2025-03-16 17:17:30 +0530

    indentation

--- Day 14/Russian_Doll_Envelopes_LC354.py
+++ Day 14/Russian_Doll_Envelopes_LC354.py
@@ -1,14 +1,14 @@
 from bisect import bisect_left
 
 def maxEnvelopes(self, envelopes) -> int:
-        n = len(envelopes)
-        envelopes.sort(key=lambda x : (x[0],-x[1]))
-        hts = [e[1] for e in envelopes]
-        res = []
-        for ht in hts:
-            pos = bisect_left(res, ht)
-            if pos == len(res):
-                res.append(ht)
-            else:
-                res[pos] = ht
-        return len(res)
+    n = len(envelopes)
+    envelopes.sort(key=lambda x : (x[0],-x[1]))
+    hts = [e[1] for e in envelopes]
+    res = []
+    for ht in hts:
+        pos = bisect_left(res, ht)
+        if pos == len(res):
+            res.append(ht)
+        else:
+            res[pos] = ht
+    return len(res)
